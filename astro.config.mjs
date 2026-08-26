78bd19511e5157fb585f94219739663cbd219364
Luis Lazo <luis.lazo.rios1@gmail.com>
2026-08-26 04:16:24 -0400

    Refactor astro.config.mjs by removing plugins
    Removed unused plugins and updated output setting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
-import typography from '@tailwindcss/typography';
 import Icons from 'unplugin-icons/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
- site: 'https://luislazorios.github.io',
-  
-  // Agrega el nombre exacto de tu repositorio con una barra al inicio
+  site: 'https://luislazorios.github.io',
   base: '/pilligua',
+  output: 'static', // Genera HTML/CSS/JS estático compatible con GitHub Pages
   integrations: [mdx()],
   vite: {
     plugins: [
-      basicSsl(),
       tailwindcss(),
       Icons({
         compiler: 'astro',
       }),
     ],
   },
-  adapter: cloudflare({
-    imageService: 'compile',
-  }),
 });
